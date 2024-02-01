0a1687934fead47316214fe2dee1d0acaa5e0b4b
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-02-01 16:49:30 +0100

    update

--- .slicemachine/assets/slices/Image/index.stories.js
+++ .slicemachine/assets/slices/Image/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":1080,"height":1618},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587905069134-008460d7a636?rect=0%2C0%2C3934%2C5894&w=1080&h=1618"},"size":"large-100","caption":[{"type":"paragraph","text":"Non elit incididunt proident dolor adipisicing sint enim amet ullamco. Sunt sit magna eiusmod deserunt incididunt laborum fugiat duis sint voluptate reprehenderit. Velit pariatur Lorem laborum sit.","spans":[]}]},"id":"_Default","slice_type":"image"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":720,"height":1080},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1589321578146-4c1ba445cc88?rect=0%2C0%2C3168%2C4752&w=720&h=1080"},"size":"large-100","caption":[{"type":"paragraph","text":"Consectetur in pariatur anim est ut duis sit enim qui cupidatat dolore ad. Occaecat non deserunt excepteur sit est eu Lorem Lorem Lorem est sint Lorem ut Lorem ad.","spans":[]}]},"id":"_Default","slice_type":"image"}} />
 _Default.storyName = ''
