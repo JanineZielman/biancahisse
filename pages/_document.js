b2c1a8c310fdb76fd27058ed1eeb44b6e011c7a9
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-02-09 22:26:47 +0100

    update

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,13 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fff"/>
-        <meta name="msapplication-TileColor" content="#fff"/>
-        <meta name="theme-color" content="#ffffff"></meta> */}
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ceff00"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
